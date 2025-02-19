5dd6cf047c0420c5ae08b6ba33223d65a53b9742
user <claudialin729@gmial.com>
2025-02-19 21:45:05 +0800

    add 物件導向 BMI Person類別

--- app/src/main/java/com/example/myapplication/Hello.kt
+++ app/src/main/java/com/example/myapplication/Hello.kt
@@ -11,9 +11,19 @@ fun main(){
     val people = Person()
     people.Hello() //people建立後會留下記憶體的住址，因此未來要尋找Person物件時，就使用people來呼叫所設計的Hello方法
 
+    //將bmi的w和h的值送進Person物件的內容裡，Person的身體裡面就會針對給的值做運算
+    people.w = 52.3f
+    people.h = 1.56f
+    println(people.bmi())
 
+    /*
+    val weight = 52.3f
+    val height = 1.56f
+    val bmi = weight / (height*height)
+    println("BMI:$bmi")
+    */
 
-
+    /*
     println("Hello Kotlin!")
     val age = 20 //就算年齡是2或20等小的數字，還是盡量使用int來標示，不用特意使用到Short/Byte來表示
     println(age.inc()) //值+1 但不影響本身的age值
@@ -28,8 +38,8 @@ fun main(){
 
     //Kotlin的浮點數資料型態: Float(單精確浮點數,32位元), Double(雙精確浮點數,64位元)
     //Kotlin一般會將帶有小數點的數值訂為Double，但也可以自己手動變為float。ex: val weight = 65.5f
-    val weight = 65.5 //Double型態
-    val weight2:Float = 65.5f //未加上.toFloat()前，會有錯誤，因為系統看到帶有小數點的值，會判斷為double
+    //val weight = 65.5 //Double型態
+    //val weight2:Float = 65.5f //未加上.toFloat()前，會有錯誤，因為系統看到帶有小數點的值，會判斷為double
     //較常使用直接產生一個浮點數的型態 val weight = 65.5f
 
     //另一種資料類型Boolean: true,false(只有這兩種可寫的值)
@@ -42,6 +52,7 @@ fun main(){
     println(name.get(0))
     println(name)
     */
+     */
 }
 
 // 可使用Template快速樣本 迅速產生main function -> 先打main，按下tab自動產生main function
--- app/src/main/java/com/example/myapplication/Person.kt
+++ app/src/main/java/com/example/myapplication/Person.kt
@@ -1,6 +1,15 @@
 package com.example.myapplication
 
 class Person {
+    //在此Person類別不只有println出Hello World的功能，同時也可以被賦予儲存資料的功能
+    //定義兩個變數的"資料"(與Hello.kt的main內的宣告(val weight = 52.3f)不同
+    var w:Float = 0f
+    var h:Float = 0f
+
+    fun bmi() : Float{ //程式碼在運算完bmi之後，會回傳一個Float的浮點數(bmi)
+        val bmi = w / ( h*h )
+        return bmi
+    }
 
     fun Hello(){
         println("Hello I'm Claudia")
