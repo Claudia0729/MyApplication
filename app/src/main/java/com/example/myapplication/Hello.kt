9aa6cd2400dad035e79bb29eb00efdd5ae0be269
user <claudialin729@gmial.com>
2025-02-27 12:30:48 +0800

    add Hello_world上課內容拆分資料夾與檔案

--- app/src/main/java/com/example/myapplication/Hello.kt
+++ app/src/main/java/com/example/myapplication/Hello.kt
@@ -1,10 +1,10 @@
 package com.example.myapplication
-//Kotlin的main函式不用像Java一樣寫在類別裡面，可寫在class外面
+
 //一個類別 名為Hello
 class Hello {
 }
 
-//第一層 First Label，也為程式的執行點(進入點)Main Function
+
 fun main(){
     //另類的Hello World-> 使用Person類別
     //將一個檔案裏面的類別(Person.類別)給生出來
@@ -63,4 +63,4 @@ fun main(){
      */
 }
 
-// 可使用Template快速樣本 迅速產生main function -> 先打main，按下tab自動產生main function
+
---
+++ app/src/main/java/com/example/myapplication/Hello_world.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication
+//Kotlin的main函式不用像Java一樣寫在類別裡面，可寫在class外面
+//第一層 First Label，也為程式的執行點(進入點)Main Function
+// 可使用Template快速樣本 迅速產生main function -> 先打main，按下tab自動產生main function
+fun main(){
+    println("Hello world!")
+}
