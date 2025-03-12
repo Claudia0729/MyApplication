38939a47e53d6c0e73414dd55ff4e0bb4411affe
user <claudialin729@gmial.com>
2025-03-12 17:08:03 +0800

    add 撲克牌的程式(加上poker symbol unicode)和針對random_for_if_猜數字(增加了使用when的方法)

--- app/src/main/java/com/example/myapplication/Random_For_IF_猜數字.kt
+++ app/src/main/java/com/example/myapplication/Random_For_IF_猜數字.kt
@@ -13,7 +13,22 @@ fun main() {
 
         println("The number you enter is $num")
 
-        if ( num < secret ){
+        val message = when {//由於when必須回傳一個值，若把break(不是值，且不會回傳資料)放入else最後，Kotlin會有錯誤。
+            num > secret -> "Please enter a smaller number!"
+            num < secret -> "Please enter a bigger number!"
+            else -> {//只要把要回傳給使用者的東西放在最後一行即可
+                bingo = true
+                "You got it!!"//此時you got it後還要做bingo和print訊息和跳出迴圈
+            }
+        }
+        println(message)//因此先輸出message，在確認bingo的值，來判斷要不要離開迴圈
+        if ( bingo ){
+            break
+        }
+        /*if ( !bingo ){//使用驚嘆號作為取反的用途，表若bingo=false(--得+)，if才會被執行。等同於bingo == false的簡寫
+            println("The correct number is $secret")
+        }*/
+        /*if ( num < secret ){
             println("Please enter a bigger number!")
         }
         else if (num > secret){
@@ -23,7 +38,7 @@ fun main() {
             println("You got it!!")
             bingo = true
             break //若猜對了，那就直接離開(結束)for迴圈，為避免繼續執行剩下次數
-        }
+        }*/
     }
     if ( !bingo ){//使用驚嘆號作為取反的用途，表若bingo=false(--得+)，if才會被執行。等同於bingo == false的簡寫
         println("The correct number is $secret")
