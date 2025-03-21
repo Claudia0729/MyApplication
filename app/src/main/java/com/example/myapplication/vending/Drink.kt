664a92a99e6f206a2a153028f2bf900e2b688b0f
user <claudialin729@gmial.com>
2025-03-21 13:05:11 +0800

    add 使用Extensions擴充功能的用法，增加兩個方法分別為off20()和validate()

--- app/src/main/java/com/example/myapplication/vending/Drink.kt
+++ app/src/main/java/com/example/myapplication/vending/Drink.kt
@@ -2,6 +2,9 @@ package com.example.myapplication.vending
 //此時還未加上冒號的類別(所有類別)，代表繼承的是一個物件(Object)類別，非真正繼承父類別的繼承
 //在類別中，若想進行覆寫父類別的方法，可使用ctrl+O。可得知，在Kotlin中，沒有繼承任何父類別時，會繼承Kotlin的Any類別。
 //為了讓簡單的類別Drink具備一些基礎性的功能，如:toString()等，可在類別最前面加上data。此時的類別會幫忙做一些額外的事情，包含如:覆寫toString()等方法
+/*Extensions(擴充功能):讓一些類別有額外的功能。但有可能因為沒有類別的原始碼，也就是要寫的功能可能是利用他人已寫好的類別，拿來使用來做我要的功能。不僅
+可用於內建的類別(如:String)上，自己寫的類別裡也可使用。只要將擴充的內容寫在第一層即可，沒有規定一定要在main的前或後。
+*/
 data class Drink( val name:String, val sugar:Int,val price:Int ) {
     /*override fun toString(): String {//會回傳一個String資料，使用此方法才能正確將物件的屬性印出來。
         return "$name $sugar $price"//這邊return的東西可自行修改
--- app/src/main/java/com/example/myapplication/vending/Map_集合.kt
+++ app/src/main/java/com/example/myapplication/vending/Map_集合.kt
@@ -1,6 +1,19 @@
 package com.example.myapplication.vending
 //Map特性: (1) 不重複 (2)map裡面要放的東西為"一對一或一組"又或 "一組資料:key-value"
+
+//若想增加一個密碼驗證的功能，不一定要找字串類別來做擴充，而是可直接使用fun設計，方法可用字串類別(String)來命名，要設計的方法可在String.後面寫上方法名稱
+//.後面的方法名稱第一個字一定要是英文字母
+fun String.validate() : Boolean{
+    return this.length >= 6//若要在字串String裡面拿到字串的本身自己，要使用到this，這樣可拿到字串本身裡面的內容。
+}
+fun Drink.off20() : Drink{//當遇到活動飲品有打8折時，因此透過自己設計的Drink類別的off20方法，會回傳一個新的飲品
+    return this.copy(price = (this.price * 0.8).toInt())//因為原本的this.price * 0.8會是一個Int*Double，因此會有錯誤，那就將this的價格括起來toInt。
+    //此時的this就是tea這個Drink物件Drink("Black tea", 50, 30)，this的核心作用是讓off20()方法知道它正在處理哪個Drink。
+}
 fun main() {
+    val s = "12345"
+    println(s.validate())//透過擴充出來的validate方法，在字串上面進行使用。
+
     var tea = Drink("Black tea", 50, 30)
     println(tea)/*此時的print印出來會是一個類別的參照資料型態(com.example.myapplication.vending.Drink@7e0ea639)
     參照資料型態即為"物件的資訊"，並沒有將tea物件的東西印出來。因此可能還需要在Drink.kt裡進行覆寫
@@ -10,6 +23,8 @@ fun main() {
     /*tea仍有方法可用的原因為，在設計一類別時，若沒有繼承任何父類別的時候，會自動幫程式繼承Object(java.lang.Object)(在Java語言中)
         像toString()方法，若未來希望直接印出來就有資料，就需要覆寫父類別Object的toString()方法。
      */
+    val discounttea = tea.off20()
+    println(discounttea)
     val tea2 = tea.copy(sugar = 20)//透過第一個類別產生出的tea物件，來呼叫copy的指令，在對身上的屬性進行微調。
     println(tea2)
 
