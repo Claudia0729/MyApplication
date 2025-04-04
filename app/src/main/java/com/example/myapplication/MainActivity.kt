1a847e2e6f0dcd1bc28d40a6255ace18469cc509
user <claudialin729@gmial.com>
2025-04-04 15:48:04 +0800

    add Android Compose元件的介紹與筆記

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyApplicationTheme {
+            MyApplicationTheme {//ctrl+b可進到Theme(路徑為:ui.theme裡面)
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -40,8 +40,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
+fun GreetingPreview() {//為了與我們未來所設計的app長相相同，會有一個預覽可以看
+    MyApplicationTheme {//Theme:主題->包含app的配色、文字的屬性狀態等，
         Greeting("Android")
     }
 }
